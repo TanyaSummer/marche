adb1ce0fd058b23e87284ec5b9438611c8e99340
TanyaSummer <89299874440@mail.ru>
2021-05-24 16:24:08 +0300

    add top-slider section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,9 @@
 $(function(){
-  // $('body').hide();
+  $('.top-slider__inner').slick({
+    dots:true,
+    arrows:false,
+    fade:true,
+    // autoplay:true,
+    // autoplaySpead:2000
+  });
 });
