93272234e927e2b5e823f0beffb99c0f389075b2
TanyaSummer <89299874440@mail.ru>
2021-09-04 13:10:31 +0300

    added adaptive product, shop, contacts page

--- app/js/main.js
+++ app/js/main.js
@@ -6,6 +6,10 @@ $(function(){
     $('.menu__list').toggleClass('menu__list--activ')
   });
 
+  $('.shop__filters-btn').on('click', function(){
+    $('.shop__filters').slideToggle();
+  })
+
   $('.footer-top__title-roll').on('click', function() {
     $(this).next().slideToggle();
     $(this).toggleClass('active');
@@ -42,7 +46,15 @@ $(function(){
     asNavFor:'.product-slide__thumb',
     draggable: false,
     arrows: false,
-    fade: true
+    fade: true,
+    responsive:[
+      {
+        breakpoint: 1051,
+        settings: {
+          draggable: true,
+        }
+      },
+    ]
   });
 
   $('.shop-content__filter-btn').on('click', function(){
@@ -52,10 +64,12 @@ $(function(){
 
   $('.button-list').on('click', function(){
     $('.product-item').addClass('product-item--list');
+    $('.shop-content__inner').addClass('shop-content__nogrid');
   });
 
   $('.button-grid').on('click', function(){
     $('.product-item').removeClass('product-item--list');
+     $('.shop-content__inner').removeClass('shop-content__nogrid');
   });
   
   $('.select-style, .product-one__item-num').styler();
